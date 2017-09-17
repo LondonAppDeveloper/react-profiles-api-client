d7ee167d381e0a7c6e09d8cae21e023b2daebb0a
Mark Winterbottom <mark@londonappdeveloper.com>
2017-09-17 13:38:12 +0100

    Added login page.

---
+++ src/containers/LoginPage.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class LoginPage extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Login Page</h2>
+      </div>
+    );
+  }
+}
+
+export default LoginPage;
