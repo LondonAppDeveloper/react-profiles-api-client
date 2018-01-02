1093112467112a3fd4641a242f2fcd987d5d830e
Mark Winterbottom <mark@londonappdeveloper.com>
2018-01-02 08:16:09 +0000

    New project.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import registerServiceWorker from './registerServiceWorker';
-import 'semantic-ui-css/semantic.min.css'
-
 import './index.css';
-import App from './containers/App';
-
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
